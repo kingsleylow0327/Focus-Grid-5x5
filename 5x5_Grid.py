d7882d4c39e04818631814d1337f5cdf32870c82
kingsleylow0327 <kingsleylow94@gmail.com>
2020-02-26 21:16:15 +0800

    added 5x5 grid algo

--- 5x5_Grid.py
+++ 5x5_Grid.py
@@ -1,1 +1,11 @@
 #Starting the Program
+import random
+llist = ["{:02d}".format(i) for i in range(1, 26)]
+random.shuffle(llist)
+
+final = []
+for i in range(5):
+  mini = llist[i*5:5+i*5]
+  print(mini)
+  final.append(mini)
+
